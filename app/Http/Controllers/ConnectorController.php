9e676653c5b6aa40b20e1cd1eee5da3b47719344
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-10 15:15:03 +0000

    feat : add controller connector

---
+++ app/Http/Controllers/ConnectorController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Connector;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreConnectorRequest;
+use App\Http\Requests\UpdateConnectorRequest;
+
+class ConnectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Connector::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $connector = Connector::create([
+            'name' => $request->name,
+            'description' => $request->description,
+
+        ]);
+
+
+        return response()->json([
+            'message'=>'connector registered successfully',
+            'connector'=>$connector
+        ]);
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Connector $connector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateConnectorRequest $request, Connector $connector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Connector $connector)
+    {
+        //
+    }
+}
