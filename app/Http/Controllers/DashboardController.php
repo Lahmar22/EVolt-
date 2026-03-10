6273f7579f305c9aabe53f9fa3e1f20197e3de13
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-10 15:16:05 +0000

    feat : update controller dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,14 +5,56 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        $user = Auth()->user();
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreConnectorRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->nom,
+            'email' => $request->regemail,
+            'password'=> Hash::make($request->regPassword),
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
 
         return response()->json([
-            'user' => $user,
+            'message'=>'User registered successfully',
+            'user'=>$user,
+            'token'=>$token
         ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Connector $connector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateConnectorRequest $request, Connector $connector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Connector $connector)
+    {
+        //
     }
 }
