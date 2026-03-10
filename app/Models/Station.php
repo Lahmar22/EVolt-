d51af0af6dfb07a0093445865f1d04b1e8c42b71
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-10 15:17:10 +0000

    feat : add the model

---
+++ app/Models/Station.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Station extends Model
+{
+    /** @use HasFactory<\Database\Factories\StationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'location',
+        'power_kw',
+        'status',
+        'connector_id',
+    ];
+}
