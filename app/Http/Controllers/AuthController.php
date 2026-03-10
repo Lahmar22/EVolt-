d66dbcbba2569c9ac1cd85a4e170a61e1da6d0f0
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-10 11:33:46 +0000

    feat : add controller auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email','password');
+
+        if (!Auth::attempt($credentials)) {
+
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ],401);
+
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function register(Request $request){
+        $request->validate([
+            'nom' => 'required|string|max:255',
+            'regemail' => 'required|string|email|max:255|unique:users,email',
+            'regPassword' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->nom,
+            'email' => $request->regemail,
+            'password'=> Hash::make($request->regPassword),
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'message'=>'User registered successfully',
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+}
