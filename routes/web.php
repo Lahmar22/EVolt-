d51b1cb3347d94cc464ada86f05c23d17b9a2f6e
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-10 11:37:58 +0000

    feat : update the route web for the page blade

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function() {
+    return view('welcome'); 
+})->name('login');
+
+
+
+
+Route::get('/dashboard', function(){
+    return view('user/dashboard');
+})->name('dashboard');
