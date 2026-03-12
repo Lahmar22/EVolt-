52f769f91258180ed388205481e7acbe911c1484
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-12 13:44:27 +0000

    feat : update connector model

--- app/Models/Connector.php
+++ app/Models/Connector.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Station;
 
 class Connector extends Model
 {
@@ -13,5 +14,11 @@ class Connector extends Model
     protected $fillable = [
         'name',
         'description',
+        'station_id'    
     ];
+
+    public function station()
+    {
+        return $this->belongsTo(Station::class);
+    }
 }
