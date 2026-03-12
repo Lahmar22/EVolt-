794de4cc69ec2bb24170b42234f64958f39d97d6
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-12 13:52:49 +0000

    feat : update the routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,19 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ConnectorController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StationController;
+use App\Http\Controllers\ReservationController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/user', [DashboardController::class, 'index']);
+    Route::apiResource('connector', ConnectorController::class);
+    Route::apiResource('station', StationController::class);
+    Route::apiResource('reservation', ReservationController::class);
 });
+
+Route::apiResource('user', DashboardController::class);
