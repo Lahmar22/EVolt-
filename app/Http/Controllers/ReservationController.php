f0a502c250f335c714498f34c467a7401f5178fa
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-12 13:42:35 +0000

    feat : add reservation controller

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use App\Http\Requests\StoreReservationRequest;
+use App\Http\Requests\UpdateReservationRequest;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $reservations = Reservation::with(['user', 'station'])->get();  
+
+        return $reservations;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $reservation = Reservation::create([
+            'start_time' => $request->start_time,
+            'duration_minutes' => $request->duration_minutes,
+            'status' => $request->status,
+            'utilisateur_id' => $request->utilisateur_id,
+            'station_id' => $request->station_id,
+        ]);
+
+        return response()->json([
+            'reservation' => $reservation
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Reservation $reservation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReservationRequest $request, Reservation $reservation)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Reservation $reservation)
+    {
+        $reservation->destroy();
+        
+        return response()->json([
+            'message' => 'destroy successfully'
+        ]);
+
+    }
+}
